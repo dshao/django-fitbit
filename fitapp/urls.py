f4c9f6fe06b2b9ea9426514229d377d1ee98f261
Danying Shao <danying.shao@gmail.com>
2023-09-12 09:55:55 -0400

    Update urls.py

--- fitapp/urls.py
+++ fitapp/urls.py
@@ -1,20 +1,20 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 
 urlpatterns = [
     # OAuth authentication
-    url(r'^login/$', views.login, name='fitbit-login'),
-    url(r'^complete/$', views.complete, name='fitbit-complete'),
-    url(r'^error/$', views.error, name='fitbit-error'),
-    url(r'^logout/$', views.logout, name='fitbit-logout'),
+    path('login/', views.login, name='fitbit-login'),
+    path('complete/', views.complete, name='fitbit-complete'),
+    path('error/', views.error, name='fitbit-error'),
+    path('logout/', views.logout, name='fitbit-logout'),
 
     # Subscriber callback for near realtime updates
-    url(r'^update/$', views.update, name='fitbit-update'),
+    path('update/', views.update, name='fitbit-update'),
 
     # Fitbit data retrieval
-    url(r'^get_data/(?P<category>[\w]+)/(?P<resource>[/\w]+)/$',
+    path('get_data/<str:category>/<str:resource>',
         views.get_data, name='fitbit-data'),
-    url(r'^get_steps/$', views.get_steps, name='fitbit-steps')
+    path('get_steps/', views.get_steps, name='fitbit-steps')
 ]
